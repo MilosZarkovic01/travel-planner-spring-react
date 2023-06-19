5592a53e32f212c19c39cdb4a2581b49adfd0a58
MilosZarkovic01 <miloszarkovic01@gmail.com>
2023-06-19 14:30:06 -0700

    Ready for deployment: full aplication

---
+++ TravelPlanner Front/travelplanner/src/components/Navbar.js
@@ -0,0 +1,65 @@
+import React, { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
+import './Navbar.css';
+
+function Navbar() {
+    const [click, setClick] = useState(false);
+    const [button, setButton] = useState(true);
+
+    const handleClick = () => setClick(!click);
+    const closeMobileMenu = () => setClick(false);
+
+    const showButton = () => {
+        if (window.innerWidth <= 960) {
+            setButton(false);
+        } else {
+            setButton(true);
+        }
+    };
+
+    useEffect(() => {
+        showButton();
+    }, []);
+
+    window.addEventListener('resize', showButton);
+
+    return (
+        <>
+            <nav className='navbar'>
+                <div className='navbar-container'>
+                    <Link to='/' className='navbar-logo' onClick={closeMobileMenu}>
+                        TravelPLANNER 
+                        <i class='fas fa-globe-africa fa-spin'></i>
+                    </Link>
+                    <div className='menu-icon' onClick={handleClick}>
+                        <i className={click ? 'fas fa-times' : 'fas fa-bars'} />
+                    </div>
+                    <ul className={click ? 'nav-menu active' : 'nav-menu'}>
+                        <li className='nav-item'>
+                            <Link to='/' className='nav-links' onClick={closeMobileMenu}>
+                                Home
+                            </Link>
+                        </li>
+                        <li className='nav-item'>
+                            <Link to='/destinations' className='nav-links' onClick={closeMobileMenu}>
+                                Destinations
+                            </Link>
+                        </li>
+                        <li className='nav-item'>
+                            <Link to='/my-profil' className='nav-links' onClick={closeMobileMenu}>
+                                My Profil
+                            </Link>
+                        </li>
+                        <li>
+                            <Link to='/login' className='nav-links' onClick={closeMobileMenu}>
+                                Logout
+                            </Link>
+                        </li>
+                    </ul>
+                </div>
+            </nav>
+        </>
+    );
+}
+
+export default Navbar;
